3a377c3eb5a9a291641e217b2af4471605a7c701
AndreaRusconi <andrearusconi1@libero.it>
2017-11-03 16:54:35 +0100

    tasto filtro

--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/MainActivity.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.user.whoplays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+       getMenuInflater().inflate(R.menu.filter_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        if(item.getItemId()==R.id.menu_filter){
+           Toast.makeText(this, "filtro selezionato", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
