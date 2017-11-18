6dac9dd0059f12bfd399c216bdeeaccf504ccf67
filodemoro <filippo.demoro@gmail.com>
2017-11-18 18:03:31 +0100

    daje

--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/FilterFragment.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/FilterFragment.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
 public class FilterFragment extends Fragment implements OnClickListener{
 
-    private TextView testoProva;
+    private TextView filtra;
     private Button provaBottone;
+    private Spinner spinnerOrder;
+    private Spinner spinnerMatchType;
+    private Spinner spinnerAdsOf;
+    private Spinner spinnerSearchType;
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,15 +29,36 @@ public class FilterFragment extends Fragment implements OnClickListener{
 
         View view = inflater.inflate(R.layout.fragment_filter, container, false);
 
-        testoProva = (TextView) view.findViewById(R.id.testoProva);
-        provaBottone = (Button) view.findViewById(R.id.provaBottone);
-        provaBottone.setOnClickListener(this);
+            filtra = (TextView) view.findViewById(R.id.filtra);
+            spinnerOrder = (Spinner) view.findViewById(R.id.spinnerOrder);
+            spinnerMatchType = (Spinner) view.findViewById(R.id.spinnerMatchType);
+            spinnerSearchType = (Spinner) view.findViewById(R.id.spinnerSearchType);
+            spinnerAdsOf = (Spinner) view.findViewById(R.id.spinnerAdsOf);
+            provaBottone = (Button) view.findViewById(R.id.provaBottone);
+
+
+        ArrayAdapter<CharSequence> adapter0 = ArrayAdapter.createFromResource(getActivity(), R.array.array_order, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(), R.array.array_match_type, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(), R.array.array_search_type, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(getActivity(), R.array.array_ads_of, android.R.layout.simple_spinner_item);
+
+        adapter0.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerOrder.setAdapter(adapter0);
+        spinnerMatchType.setAdapter(adapter1);
+        spinnerSearchType.setAdapter(adapter2);
+        spinnerAdsOf.setAdapter(adapter3);
+
+            provaBottone.setOnClickListener(this);
 
         return view;
     }
 
     public void changeText() {
-        testoProva.setText("Rusconi Coglione");
+        filtra.setText("Rusconi Coglione");
     }
 
     @Override
