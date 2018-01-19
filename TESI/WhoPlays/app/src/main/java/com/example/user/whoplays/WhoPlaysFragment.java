91d8d8f3b5d8a756aa9ad1afb5a72fb0af98f0b0
AndreaRusconi <andrearusconi1@libero.it>
2018-01-19 17:01:43 +0100

    listview

--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/WhoPlaysFragment.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/WhoPlaysFragment.java
@@ -10,7 +10,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by User on 01/12/2017.
@@ -18,10 +29,15 @@ import android.widget.TextView;
 
 public class WhoPlaysFragment extends Fragment {
 
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList <String> list= new ArrayList<>();
+    ArrayAdapter <String> adapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
 
     @Nullable
@@ -29,6 +45,39 @@ public class WhoPlaysFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_who_plays, container, false);
         getActivity().setTitle(R.string.app_name);
+        listView =  view.findViewById(R.id.listViewWhoPlays);
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line,list);
+        listView.setAdapter(adapter);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                Map value= dataSnapshot.getValue(Map.class);
+                list.add(value.toString());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
 
