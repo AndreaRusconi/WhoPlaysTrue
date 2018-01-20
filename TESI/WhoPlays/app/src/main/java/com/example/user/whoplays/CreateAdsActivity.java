edc3f3b4b9d8dec42ca935a163a19abec17a3ea0
filodemoro <filippo.demoro@gmail.com>
2018-01-20 14:12:59 +0100

    Revert "Inizio calendario e grafica New_ads"
    This reverts commit 26698c253002723144a4e5219b8725c051d4513b.

--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/CreateAdsActivity.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/CreateAdsActivity.java
@@ -12,7 +12,6 @@ import android.app.Dialog;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
