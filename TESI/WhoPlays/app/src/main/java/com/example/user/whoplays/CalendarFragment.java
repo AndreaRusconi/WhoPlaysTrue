ee15d86dfd88e45b4b87378f029b42d11c4ebaf1
AndreaRusconi <andrearusconi1@libero.it>
2018-01-22 13:33:44 +0100

    peppoo

--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/CalendarFragment.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.user.whoplays;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,28 +11,41 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Calendar;
 
 /**
  * Created by User on 01/12/2017.
  */
 
 public class CalendarFragment extends Fragment {
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getActivity().setTitle(R.string.calendar);
-
-        return inflater.inflate(R.layout.fragment_calendar,null);
-    }
+    DatabaseReference databaseReference;
+    CalendarView calendar;
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        calendar = (CalendarView) view.findViewById(R.id.calendar_match); // get the reference of CalendarView
 
+        calendar.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
+        calendar.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
+        calendar.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
+        // perform setOnDateChangeListener event on CalendarView
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // display the selected date by using a toast
+                Toast.makeText(getContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
 
-
+        return inflater.inflate(R.layout.fragment_calendar,container,false);
     }
+
 
 }
