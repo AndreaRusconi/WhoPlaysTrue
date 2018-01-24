98285c1792a3c8f7c6b83729b9079ea32922cbc6
AndreaRusconi <andrearusconi1@libero.it>
2018-01-24 13:03:15 +0100

    settings1

---
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/ModPassActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.whoplays;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 24/01/2018.
+ */
+
+public class ModPassActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_newpassword);
+        super.onCreate(savedInstanceState);
+    }
+}
--- TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/SettingsFragment.java
+++ TESI/WhoPlays/app/src/main/java/com/example/user/whoplays/SettingsFragment.java
@@ -1,11 +1,17 @@
 package com.example.user.whoplays;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * Created by User on 01/12/2017.
@@ -13,10 +19,55 @@ import android.view.ViewGroup;
 
 public class SettingsFragment extends Fragment{
 
+    private Button applica;
+    private Button modpass;
+    private Button elimina;
+    private EditText email;
+
+    private DatabaseReference databaseReference;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle(R.string.settings);
-        return inflater.inflate(R.layout.fragment_settings,null);
+
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        applica = (Button) view.findViewById(R.id.button_applica);
+        modpass = (Button) view.findViewById(R.id.button_modifica_password);
+        elimina = (Button) view.findViewById(R.id.button_elimina_account);
+        email = (EditText) view.findViewById(R.id.edit_text_email);
+
+
+        applica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String taken =  email.getText().toString();
+                if (!taken.isEmpty()) {
+                    startActivity(new Intent(getContext(), WhoPlaysActivity.class));
+                    Toast.makeText(getContext(), taken, Toast.LENGTH_LONG).show();
+
+                }
+                else
+                    Toast.makeText(getContext(), "nessuna mail inserita", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        modpass.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view){
+
+                startActivity(new Intent(getContext(), ModPassActivity.class));
+            }
+        });
+
+
+
+
+        return view;
     }
+
+
 }
+
